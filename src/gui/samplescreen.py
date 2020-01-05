53982a3bf3ae03fd7ac8081a764e3741e7e6cabd
Adanna Obibuaku <32201807+runnily@users.noreply.github.com>
2020-01-05 01:51:26 +0000

    Added a frame
    Added updates to make it look more like GUI design

--- src/gui/samplescreen.py
+++ src/gui/samplescreen.py
@@ -5,99 +5,101 @@ from utils import Screen
 
 class ConnectionStatus(QWidget):
 
+    def __init__(self, _connection_status_header="", _connection_status_body = [],
+                 _connection_status_footer=""):
+        """
 
-    def __init__(self, _connection_status_1= "", _connection_status_2 = "", _connection_status_3 ="", _connection_status_4 = ""):
-
-
+        :param _connection_status_header: The header of the status
+        :param _connection_status_body: Holds an array of status
+        :param _connection_status_footer: the footer for the status
+        """
         super(ConnectionStatus, self).__init__()
-        self._connection_status_1 = _connection_status_1
-        self._connection_status_2 = _connection_status_2
-        self._connection_status_3 = _connection_status_3
-        self._connection_status_4 = _connection_status_4
+        self._connection_status_header = _connection_status_header
+        self._connection_status_body = _connection_status_body
+        self._connection_status_footer = _connection_status_footer
 
-        self._connection_status1_label = QLabel()
-        self._connection_status2_label = QLabel()
-        self._connection_status3_label = QLabel()
-        self._connection_status4_label = QLabel()
+        self._layout = QVBoxLayout()  # create a QVboxLayout for the frame
+        self._layout_l = QGridLayout()  # create a QVboxLayout for the frame
+        self._connection_status_header_label = QLabel()  # create a label for the frame
+        self._connection_status_footer_label = QLabel()
 
+        self._layout.addWidget(self._connection_status_header_label)
+        self._status_dict = {}  # A status dictionary to store all the status of the dictionary
 
+        # makes a directory to connect a label to connection status.
+        self._frame = QFrame();
+        for x in self._connection_status_body:
+            label = QLabel()
+            self._status_dict[label] = x
+            self._layout_l.addWidget(label)
 
-        self._layout = QVBoxLayout()
-        if self._connection_status_1 != "" :self._layout.addWidget(self._connection_status1_label)
-        if self._connection_status_2 != "" :self._layout.addWidget(self._connection_status2_label)
-        if self._connection_status_3 != "" :self._layout.addWidget(self._connection_status3_label)
-        if self._connection_status_4 != "" :self._layout.addWidget(self._connection_status4_label)
-        self.setLayout(self._layout)
+        self._frame.setLayout(self._layout_l)
+
+        self._layout.addWidget(self._frame)
+        self._layout.addWidget(self._connection_status_footer_label)
+
+        self.setLayout(self._layout)  # then set the layout widget
+        self._set_style()
 
     def _set_style(self):
 
-        self._connection_status1_label.setAlignment(Qt.AlignCenter)
-        self._connection_status2_label.setAlignment(Qt.AlignCenter)
-        self._connection_status3_label.setAlignment(Qt.AlignCenter)
-        self._connection_status4_label.setAlignment(Qt.AlignCenter)
+        self._layout.setSpacing(0);
 
-        self._connection_status1_label.setText(self._connection_status_1)
-        self._connection_status2_label.setText(self._connection_status_2)
-        self._connection_status3_label.setText(self._connection_status_3)
-        self._connection_status4_label.setText(self._connection_status_4)
-        #set a different style sheet
-        #pass
+        for key, value in {self._connection_status_header_label: self._connection_status_header,
+                           self._connection_status_footer_label: self._connection_status_footer}.items():
+            key.setAlignment(Qt.AlignCenter)
+            key.setText(value)
 
-        self._layout.setSpacing(0)
+        for key, value in self._status_dict.items():
+            key.setAlignment(Qt.AlignCenter)
+            key.setText(value)
+            key.setStyleSheet("""
+                font-size: 20px;
+                background-color: orange;
+                            """)
+
+        self._frame.setStyleSheet("""background-color: orange;
+                                              border: none;""")
         self.setStyleSheet("""
-                    QWidget { 
-                        background-color: rgb(8, 64, 67);
-                    }
-        """)
+            QWidget {   
+               font-family: Arial;
+               background-color: rgb(8, 64, 67);
+            }
+            
+            QFrame {
+              border: 5px solid orange;
+            }
+            
+            QLabel {
+               color: white;
+                font-family: cursive
+            }
+            """)
+
+        def update_values(self):
+            pass
 
 
-        self._connection_status2_label.setStyleSheet("QLabel { background-color: yellow;}")
-
-
-
-class Raseberrypi(Screen):
-    '''
-    This is a screen which is going to repersent the status of the raseberry pi
-    '''
-
-    def __init__(self):
-        super().__init__()
-        self._connection_status = ConnectionStatus("R Pi", "Fake Status", "Arduino")
-
-
-    def _config(self):
-        self.resize(50,50)
-        self._connection_status._set_style()
-        self._layout = QVBoxLayout()
-        self._layout.addWidget(self._connection_status)
-        self.setLayout(self._layout)
-
-        self.setStyleSheet(" self { border:1px solid rgb(0, 255, 0)};")
-
-class landStatus(Screen):
-    '''
-    This is a screen which is going to reperesent the status of the land status
-    '''
+class ConnectionStatusSample(Screen):
+    """
+    This is a screen which is going to represent the status of the raspberry pi
+    """
 
     def __init__(self):
         super().__init__()
-        self._connection_status = ConnectionStatus("LAND", "Status", "RPI")
-
+        self._raspberry_pi = ConnectionStatus("R Pi", ["Fake Status"], "Arduino")
+        self._land = ConnectionStatus("LAND", ["Fake Status"], "R Pi")
 
     def _config(self):
-        self.resize(50,50)
-        self._connection_status._set_style()
-        self._layout = QVBoxLayout()
-        self._layout.addWidget(self._connection_status)
+        self._layout = QHBoxLayout()
+        self._layout.addWidget(self._raspberry_pi)
+        self._layout.addWidget(self._land)
         self.setLayout(self._layout)
-
-        self.setStyleSheet(" self { border:1px solid rgb(0, 255, 0)};")
-
 
 
 if __name__ == "__main__":
     app = QApplication()
-    form = Raseberrypi()
+    form = ConnectionStatusSample()
     form._config()
     form.show()
     app.exec_()
